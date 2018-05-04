8127d1349b8cf5758ab7f05622974cdba68054a1
Filipe Oliveira <filipe.dmo@gmail.com>
2018-05-04 20:21:46 +0100

    changes

--- FroggerGame/README.md
+++ FroggerGame/README.md
@@ -1,8 +1,15 @@
 ## Frogger Game
 Frogger Game can be accessed by this [link](https://rawgit.com/FDMOliveira/Front-end-nanodegree-exercises/master/FroggerGame/index.html).
 
+# Instructions
 The goal of the game is to reach the water. 
 
 If you achieve it, you earn a star, but watch out: if you colide with an enemie you could loose one.
 
 Good luck and have fun.
+
+# Tools used
+In the Frogger Game development process folowing Grunt tasks were usednpm:
+ * [grunt-contrib-imagemin](https://github.com/gruntjs/grunt-contrib-imagemin) to minify image files
+ * [grunt-contrib-uglify](https://github.com/gruntjs/grunt-contrib-uglify) to minify Javascript files
+ * [grunt-contrib-cssmin](https://github.com/gruntjs/grunt-contrib-cssmin) to minify CSS files
--- FroggerGame/js/app.js
+++ FroggerGame/js/app.js
@@ -1,114 +1,127 @@
 var records = document.getElementById("records");
-    starsEarned=0;
-    
-class Enemy {
-    constructor() {   
+starsEarned=0;
+
+class Character {
+constructor(x, y, sprite) {
+    this.x=x;
+    this.y=y;
+    this.sprite=sprite;
+    this.speed=1;
+}
+}
+Character.prototype.render = function() {
+ctx.drawImage(Resources.get(this.sprite),this.x,this.y);
+};
+class Enemy extends Character {
+    constructor(x, y, sprite) {
+        super(x, y, sprite);  
     this.x=-100;
     this.y=enemiesYPositions[Math.floor(Math.random() * enemiesYPositions.length)];
-    this.speed = 1;
-    this.sprite = 'images/enemy-bug.png';
-    }
+    this.sprite='images/enemy-bug.png';
+this.speed = 1;
+}
+render() {
+    return super.prototype.render()
+}   
 };
-function initialPlayerPosition () {
-    player.y=390;
-    player.x=200;
-    }
-Enemy.prototype.update = function(dt) { 
-    this.x+=2* this.speed;
-
-    // You should multiply any movement by the dt parameter
-    // which will ensure the game runs at the same speed for
-    // all computers.
-    if (this.x>520) {
-        this.x=-100;
-        this.y = enemiesYPositions[Math.floor(Math.random() * enemiesYPositions.length)]
-    }    
-};
-
-Enemy.prototype.render = function() {
-    ctx.drawImage(Resources.get(this.sprite),this.x,this.y);
-};
-
-class Player {
-    constructor() {
+class Player extends Character {
+constructor(x, y, sprite) {
+    super(x, y, sprite);
     this.x=200;
-    this.y=390;
+    this.y=300;
+    this.sprite='images/char-boy.png';
     this.speed = 1;
-    this.sprite = 'images/char-boy.png';
-    }
 }
+render() {
+    return super.prototype.render()
+}
+}
+function initialPlayerPosition () {
+player.y=390;
+player.x=200;
+}
+Enemy.prototype.update = function(dt) { 
+this.x+=150*dt* this.speed;
+// You should multiply any movement by the dt parameter
+// which will ensure the game runs at the same speed for
+// all computers.
+if (this.x>520) {
+    super(x, y);
+    this.x=-100;
+    this.y = enemiesYPositions[Math.floor(Math.random() * enemiesYPositions.length)]
+}    
+};
 Player.prototype.update = function(){
-    if ((player.y == -35)) {
-        starsEarned++;
-        initialPlayerPosition();
-        star.classList.add("star-loaded");
-        setTimeout(function() {
-            star.classList.remove("star-loaded");
-        },1000)
-    }
-    document.getElementById("stars-number").innerHTML=starsEarned;
+if ((player.y == -35)) {
+    starsEarned++;
+    initialPlayerPosition();
+    star.classList.add("star-loaded");
+    setTimeout(function() {
+        star.classList.remove("star-loaded");
+    },1000)
 }
-Player.prototype.render = function(){
-    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+document.getElementById("stars-number").innerHTML=starsEarned;
 }
+
 Player.prototype.handleInput = function(keyCode){
-    switch(keyCode) {
-        case 'left':  if (this.x > 0)
-                          this.x-=100;
-                          break
-        case 'right': if (this.x < 400) 
-                          this.x+=100;
-                          break
-        case 'up':    if (this.y > -35)
-                          this.y-=85;
-                          break
-        case 'down':  if (this.y < 390)
-                          this.y+=85;
-                          break   
-    }
+switch(keyCode) {
+    case 'left':  if (this.x > 0)
+                      this.x-=100;
+                      break
+    case 'right': if (this.x < 400) 
+                      this.x+=100;
+                      break
+    case 'up':    if (this.y > -35)
+                      this.y-=85;
+                      break
+    case 'down':  if (this.y < 390)
+                      this.y+=85;
+                      break   
+}
 }
 var player = new Player();
 var allEnemies = [];
 var enemiesYPositions = [50,135,220];
 function getRandomArbitrary(min, max) {
-    return Math.random() * (max - min) + min;
+return Math.random() * (max - min) + min;
 }
 function enemiesSpeed() {
-    allEnemies.forEach(function(enemy) {
-        enemy.speed = getRandomArbitrary(1,2.3);
-    });
+allEnemies.forEach(function(enemy) {
+    enemy.speed = getRandomArbitrary(1,2.3);
+});
 }
 function createEnemies () {
-    for (i=0; i<3; i++) {
-        var enemy = new Enemy();
-        allEnemies.push(enemy);
-    }
+for (i=0; i<3; i++) {
+    var enemy = new Enemy();
+    allEnemies.push(enemy);
 }
-    createEnemies();
-    enemiesSpeed();
-document.addEventListener('keyup', function(e) {
-    var allowedKeys = {
-        37: 'left',
-        38: 'up',
-        39: 'right',
-        40: 'down'
-    };
+}
+createEnemies();
+enemiesSpeed();
 
-    player.handleInput(allowedKeys[e.keyCode]);
+document.addEventListener('keyup', function(e) {
+var allowedKeys = {
+    37: 'left',
+    38: 'up',
+    39: 'right',
+    40: 'down'
+};
+
+player.handleInput(allowedKeys[e.keyCode]);
 });
 function checkCollisions() {
-    allEnemies.forEach(function(enemy) {
-        var enemyxposition= Math.round(enemy.x);
-        var playerXPosition = player.x;
-        if ((player.x > enemyxposition-50) && (player.x < enemyxposition +70) 
-            && (player.y == enemy.y)) {
-                    starsEarned--;
-                initialPlayerPosition();
-                dieAlert.classList.add("die-animation");
-                setTimeout(function() {
-                    dieAlert.classList.remove("die-animation");
-                },600)
-        }
-        document.getElementById("stars-number").innerHTML=starsEarned;
-    })
+allEnemies.forEach(function(enemy) {
+    var enemyxposition= Math.round(enemy.x);
+    var playerXPosition = player.x;
+    if ((player.x > enemyxposition-50) && (player.x < enemyxposition +70) 
+        && (player.y == enemy.y)) {
+                starsEarned--;
+            initialPlayerPosition();
+            dieAlert.classList.add("die-animation");
+            setTimeout(function() {
+                dieAlert.classList.remove("die-animation");
+            },600)
+    }
+    document.getElementById("stars-number").innerHTML=starsEarned;
+})
 }
