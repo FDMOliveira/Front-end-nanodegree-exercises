62ed14ac4f2ebe3f8971aa5d18cacf81324dde48
Filipe Oliveira <filipe.dmo@gmail.com>
2018-05-22 16:53:25 +0100

    change

--- NewsAggregator/scripts/app.js
+++ NewsAggregator/scripts/app.js
@@ -24,9 +24,9 @@ APP.Main = (function() {
       isStoryDetails = false,
       commentDetails,
       i=0,
-      k=0;
-      var dataWorker = new Worker("data.js");
-      var localeData = {
+      k=0,
+      dataWorker = new Worker("/data.js"),
+      localeData = {
         data: {
           intl: {
             locales: 'en-US'
