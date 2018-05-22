651a6a66d9aab081fd5cca50a022db24e82e82a6
Filipe Oliveira <filipe.dmo@gmail.com>
2018-05-22 16:24:21 +0100

    changes

--- NewsAggregator/scripts/app.js
+++ NewsAggregator/scripts/app.js
@@ -155,7 +155,7 @@ function loadStoryBatch() {
     }
     requestAnimationFrame(loadStoryAnimation);
 }
-  dataWorker.postMessage([1])
+  dataWorker.postMessage(['1'])
   dataWorker.onmessage = function(e) {
     stories = e.data;
     loadStoryBatch();
