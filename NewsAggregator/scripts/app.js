7618c07f6ee32f82d43afea88664fc5a8988f01f
Filipe Oliveira <filipe.dmo@gmail.com>
2018-05-22 19:13:38 +0100

    changes

--- NewsAggregator/scripts/app.js
+++ NewsAggregator/scripts/app.js
@@ -14,7 +14,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-APP.Main = (function() {
+(function() {
 
   var $ = document.querySelector.bind(document),
       stories = null,
