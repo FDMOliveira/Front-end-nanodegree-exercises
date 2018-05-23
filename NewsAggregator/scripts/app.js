a9d0d7d401d6dec98d91d5bbbb12b67bb888bb6d
Filipe Oliveira <filipe.dmo@gmail.com>
2018-05-23 19:07:44 +0100

    change

--- NewsAggregator/scripts/app.js
+++ NewsAggregator/scripts/app.js
@@ -137,7 +137,7 @@
       document.body.classList.add('raised');
     else
       document.body.classList.remove('raised'); 
-    var lastElement = document.getElementById('s-'+storyChunk[storyChunk.length-1]);
+    var lastElement = $('s-'+storyChunk[storyChunk.length-1]);
     // If the last element of the array is shown, it creates a new one 
       if (($(lastElement).offset().top) > main.scrollTop) {
         loadStoryBatch();
